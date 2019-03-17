7e2d9beaf6869676c49a4ad27c4bfcbfd99504af
Eric Calasans <ericcalasans@gmail.com>
2019-03-16 22:25:43 -0300

    Cap 4 iniciado

--- hackerpair/app/Event.php
+++ hackerpair/app/Event.php
@@ -4,7 +4,22 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Event extends Model
-{
-    //
+class Event extends Model{
+    public function getNameAttribute($value){
+        $ignore = ['a', 'and', 'at', 'but', 'for', 'in', 'the', 'to', 'with'];
+
+        $name = explode(' ', $value);
+
+        $modifiedName = [];
+
+        foreach ($name as $word){
+            if(! in_array(strtolower($word), $ignore)){
+                $modifiedName[] = ucfirst($word);
+            } else {
+                $modifiedName[] = strtolower($word);
+            }
+        }
+
+        return join(' ', $modifiedName);
+    }
 }
